c71aeb5e0e9824013650d37818b009d58b35c1df
Viktoria Gusakova <vgoosakova@gmail.com>
2021-02-08 13:34:57 +0200

    Initial Commit

---
+++ Mobile development/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile development
+//
+//  Created by Viktory  on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
